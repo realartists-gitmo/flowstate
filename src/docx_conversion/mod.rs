cbe2c64a0417145c364683147acfe0f7d0b382d8
Chinmay Khaladkar <chinmaykhaladkar1@gmail.com>
2026-05-30 17:33:52 -0700

    docx export based on settings.json

--- src/docx_conversion/mod.rs
+++ src/docx_conversion/mod.rs
@@ -1,1 +1,15 @@
-pub use flowstate_docx::*;
+use std::{io, path::Path};
+
+pub use flowstate_docx::{
+  CleanAction, CleanedDocx, DocxCleanReport, DocxCleanStats, DocxConversionReport, RecognitionRule, clean_docx_bytes,
+  convert_cleaned_docx_to_document, convert_docx_bytes_to_document, convert_docx_to_db8, convert_docx_to_document, write_docx,
+};
+
+use crate::app_settings::load_document_theme;
+
+#[hotpath::measure]
+pub fn convert_db8_to_docx(input: impl AsRef<Path>, output: impl AsRef<Path>) -> io::Result<()> {
+  let mut document = flowstate_document::read_db8(input)?;
+  document.theme = load_document_theme();
+  flowstate_docx::write_docx(output, &document)
+}
