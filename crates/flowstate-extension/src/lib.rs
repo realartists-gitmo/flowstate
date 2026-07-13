988766d2e5df2099f5c52961b0caf63e616d33c3
Chinmay Khaladkar <chinmaykhaladkar1@gmail.com>
2026-07-12 22:20:06 -0700

    Define extension component interface

--- crates/flowstate-extension/src/lib.rs
+++ crates/flowstate-extension/src/lib.rs
@@ -1,8 +1,9 @@
 mod discovery;
 mod manifest;
+mod runtime;
 mod trust;
 
 pub use discovery::{DiscoveryIssue, DiscoveryResult, InstalledExtension, discover_extensions};
 pub use manifest::{ActionManifest, ExtensionManifest, ManifestError};
+pub use runtime::{CancellationHandle, ExtensionHost, HostError, Invocation, InvocationOutput, Runtime, RuntimeConfig, RuntimeError};
 pub use trust::{ComponentDigest, TrustDecision, TrustStore};
-
